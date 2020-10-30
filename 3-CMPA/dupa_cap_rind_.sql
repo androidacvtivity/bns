2bff128a3f4a6f201d6f973f203006aa5a40bf9e
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-10-30 17:29:14 +0200

    work day is finish

---
+++ 3-CMPA/dupa_cap_rind_.sql
@@ -0,0 +1,46 @@
+﻿SELECT 
+   
+         CASE WHEN D.CAPITOL = 1115 THEN 'Tabelul_1'
+              WHEN D.CAPITOL = 1116 THEN 'Tabelul_2'
+              WHEN D.CAPITOL = 1117 THEN 'Tabelul_3'
+         END 
+          AS CAPITOL,
+         D.RIND,   
+         SUM(D.COL1) AS COL1,
+         SUM(D.COL2) AS COL2,
+         SUM(D.COL3) AS COL3,
+         SUM(D.COL4) AS COL4,
+         SUM(D.COL5) AS COL5,
+         SUM(D.COL6) AS COL6,
+         SUM(D.COL7) AS COL7,
+         SUM(D.COL8) AS COL8,
+         SUM(D.COL9) AS COL9,
+         SUM(D.COL10) AS COL10,
+         SUM(D.COL11) AS COL11,
+         SUM(D.COL12) AS COL12,
+         SUM(D.COL13) AS COL13
+       
+        
+        
+FROM
+  VW_DATA_ALL_GC  D
+  
+  
+  
+WHERE
+  (D.PERIOADA IN (:pPERIOADA))  AND   
+  (D.FORM =:pFORM) AND
+  (D.FORM_VERS =:pFORM_VERS) AND 
+  (:pID_MDTABLE =:pID_MDTABLE) AND 
+  (D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%') AND
+  D.FORM IN (62)  AND
+  D.CAPITOL IN (1115,1116,1117)
+  
+  
+  GROUP BY 
+   D.CAPITOL,
+   D.RIND  
+  
+    ORDER BY 
+   D.CAPITOL,
+   D.RIND
