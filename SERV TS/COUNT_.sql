19a2c560dbbc9a88571f43030e70d73498538e98
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-11-27 11:18:42 +0200

    update

---
+++ SERV TS/COUNT_.sql
@@ -0,0 +1,20 @@
+﻿SELECT 
+             FORM,
+             CUIIO,
+             ID_MD,   
+             COUNT(CUIIO||' '||ID_MD) AS CNT
+             
+             FROM USER_EREPORTING.DATA_ALL
+             
+             WHERE 
+             
+             FORM IN (2,3,4,5) AND PERIOADA = 429
+             
+             
+             GROUP BY 
+             FORM,
+             CUIIO,
+             ID_MD 
+             
+             HAVING 
+             COUNT(CUIIO||' '||ID_MD) > 1
