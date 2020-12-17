51dcefee80059e36e5c7ef45322b1b7a7efc593c
iostream17 <17309940+iostream17@users.noreply.github.com>
2020-12-17 12:01:16 +0200

    update

---
+++ RSF3 - reprezentatilor/step2.sql
@@ -0,0 +1,18 @@
+﻿SELECT  
+  CAPITOL,
+  CAPITOL_VERS,
+  DENUMIRE,
+  ORDINE,
+  DATA_REG,
+  STATUT        
+ 
+FROM CIS2.MD_CAPITOL
+
+ORDER BY 
+
+CAPITOL,
+CAPITOL_VERS
+
+
+--Ultimul Capitol este 1131 cu versiunea 2000
+--Mie imi trebuie doua capitole adaug 1132 si 1133 ?  
