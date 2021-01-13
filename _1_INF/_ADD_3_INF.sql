c74ffa2712160ebf00aa98e0395a889f92378ea0
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-01-13 15:54:12 +0200

    update

---
+++ _1_INF/_ADD_3_INF.sql
@@ -0,0 +1,13 @@
+﻿    SELECT *
+        FROM USER_BANCU.VW_KATALOG_1_INF_2009
+        
+        
+        WHERE 
+        
+        CUIIO_VERS = 2009;
+        
+        
+        SELECT 
+        CUIIIO
+        FROM USER_BANCU.KAT_1_INF;
+        
