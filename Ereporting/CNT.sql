f24423feb411d44d07cc850bcbcf1102459702fa
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-01-26 13:30:23 +0200

    update

---
+++ Ereporting/CNT.sql
@@ -0,0 +1,14 @@
+﻿            SELECT 
+            
+            D.CUIIO
+            DA  
+            
+            FROM USER_EREPORTING.DATA_ALL D
+            
+            WHERE 
+            D.PERIOADA = :pPERIOADA 
+            AND D.FORM  = :pFORM  
+             
+            
+            ORDER BY 
+            DATA_REG DESC
