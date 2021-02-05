f164976495c882aa998202f8a41f57120ccb72df
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-02-05 10:09:54 +0200

    update

---
+++ _1_AUTO/Verified/_3_check.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+  L.CUIIO 
+FROM USER_BANCU.VW_1_AUTO_431 L LEFT 
+
+                JOIN USER_BANCU.VW_65_AUTO_1047 R ON L.CUIIO = R.CUIIO 
+                
+                WHERE 
+                
+                R.CUIIO IS NULL
