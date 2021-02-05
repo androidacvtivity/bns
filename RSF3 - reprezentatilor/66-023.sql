120e446b1886549638be12de269e9a6398925385
iostream17 <17309940+iostream17@users.noreply.github.com>
2021-02-05 09:13:25 +0200

    update

---
+++ RSF3 - reprezentatilor/66-023.sql
@@ -0,0 +1,30 @@
+﻿SELECT 
+    'Rind.'||D.RIND ||' '|| DECODE(CC.NR_COLUMN, '1', 'COL4 ', '2', 'COL5 ')--||'- valoarea rîndului trebuie sa fie pozitiv'  
+    AS REZULTAT
+FROM
+  CIS2.VW_DATA_ALL_FR D,             
+       (                                                                        
+       SELECT '1' AS NR_COLUMN FROM DUAL UNION                                  
+       SELECT '2' AS NR_COLUMN FROM DUAL                                  
+       ) CC   
+WHERE
+  (D.PERIOADA=:PERIOADA         OR :PERIOADA = -1) AND
+  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               OR :FORM = -1) AND
+  (D.FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (66)  AND
+  D.CAPITOL IN (1133) AND
+  D.RIND NOT IN ('030','060','070','080')
+  
+GROUP BY
+  CC.NR_COLUMN,D.RIND,DECODE(CC.NR_COLUMN, '1', D.COL1, '2', D.COL2)
+
+HAVING
+DECODE(CC.NR_COLUMN,  '1', D.COL1, '2', D.COL2)<0
+
+ORDER BY D.RIND
---
+++ SYS_USER/ID_USER.sql
@@ -0,0 +1,80 @@
+﻿                INSERT INTO CIS2.SYS_USER_ACCES (
+                
+                 ID_USER,
+                 CUATM,
+                 FORM,
+                 FORM_VERS,
+                 ISADMIN,
+                 ACCES_TYPE,
+                 CAPITOL_ACCES,
+                 DATA_REG  
+                
+                )
+
+
+
+  SELECT 
+                
+                 L.ID_USER,
+                 L.CUATM,
+                 L.FORM,
+                 L.FORM_VERS,
+                 L.ISADMIN,
+                 L.ACCES_TYPE,
+                 L.CAPITOL_ACCES,
+                 SYSDATE DATA_REG    
+
+
+FROM 
+
+
+(
+
+                SELECT 
+                
+                 ID_USER,
+                 CUATM,
+                 66 FORM,
+                 FORM_VERS,
+                 ISADMIN,
+                 ACCES_TYPE,
+                 CAPITOL_ACCES,
+                 SYSDATE DATA_REG    
+                
+                
+                    FROM CIS2.SYS_USER_ACCES 
+                    
+                    WHERE 
+                    
+                    FORM = 57
+                    
+                    AND FORM_VERS = 2000
+                   -- AND ID_USER NOT IN (734,693)
+                    ) L LEFT JOIN (
+                    
+                    SELECT 
+                
+                 ID_USER,
+                 CUATM,
+                 FORM,
+                 FORM_VERS,
+                 ISADMIN,
+                 ACCES_TYPE,
+                 CAPITOL_ACCES,
+                 SYSDATE DATA_REG    
+                
+                
+                    FROM CIS2.SYS_USER_ACCES 
+                    
+                    WHERE 
+                    
+                    FORM = 66
+                    
+                    ) R ON R.ID_USER = L.ID_USER 
+                    
+                    
+                    WHERE 
+                    
+                    R.ID_USER IS  NULL 
+                    
+                     AND ROWNUM < 20
