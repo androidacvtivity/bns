47b9940d10934cba2e29f2166c6e05eabbcaf849
Vitalie Bancu <VitalieBancu@intern.statistica.md>
2021-03-04 10:59:10 +0200

    update

---
+++ EI 78/BUG _04_03_2021/bug1.sql
@@ -0,0 +1,16 @@
+SELECT FC.CUIIO,
+       FC.CUIIO_VERS
+                  
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (46) AND CUIIO_VERS <= :pPERIOADA
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (46) AND FC.STATUT <> '3' 
+             
+             
+             --6263 -- 2009
+             --7012 -- 2010
+             
