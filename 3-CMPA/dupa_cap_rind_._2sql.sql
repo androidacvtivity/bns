8ec69a78e9563065b9956c97fdc7ed4e7e3ab157
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-05-13 11:52:18 +0300

    update

--- 3-CMPA/dupa_cap_rind_._2sql.sql
+++ 3-CMPA/dupa_cap_rind_._2sql.sql
@@ -7,26 +7,21 @@ SELECT
 
 
 FROM
-  VW_DATA_ALL_GC  D
+  CIS2.VW_DATA_ALL_GC  D
   
   WHERE
   
    (D.PERIOADA IN (:pPERIOADA))  AND   
    (D.FORM = 62) 
-   AND D.COL1 IS NOT NULL
- --  AND REGEXP_LIKE (D.COL1, '[[:alpha:]]')
- 
--- AND REGEXP_LIKE(D.COL1, '[[:digit:]]')
- 
- --AND REGEXP_LIKE(TO_CHAR(D.COL1), '[^0-9]+')
+  
+
  
  
    GROUP BY 
     D.UNIT_CODE,
          D.CUATM,
          D.CAPITOL,
-          D.RIND ,
-          D.COL1  
+          D.RIND
          
          
         ORDER BY 
