f54e6ba4a1b5465cde090759be75e2b8218383a2
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-05-18 13:13:08 +0300

    update

---
+++ 5 CI/ADD_CUIIO_1048/ADD_CUIIO_1048_step5_update_FINAL_VER2.sql
@@ -0,0 +1,42 @@
+UPDATE CIS.RENIM VB
+        
+ SET     (
+
+ 
+                 VB.DENUMIRE,
+                 VB.CUATM,
+                 VB.CFP,
+                 VB.CFOJ,
+               
+                 VB.CAEM2
+                                    --7
+ 
+  ) 
+            = 
+  (          
+  SELECT 
+
+ 
+                 C.DENUMIRE,
+                 C.CUATM,
+                 C.CFP,
+                 C.CFOJ,
+               
+                 C.CAEM2
+                                       --7
+ 
+  FROM USER_BANCU.KAT_RSF C
+       WHERE
+                   VB.CUIIO       = C.CUIIO AND 
+                   VB.CUIIO_VERS  = C.CUIIO_VERS
+                   
+                )   
+                WHERE EXISTS  
+                
+                (
+                    SELECT *
+                    FROM USER_BANCU.KAT_RSF C
+                   WHERE
+                   VB.CUIIO       = C.CUIIO AND 
+                   VB.CUIIO_VERS  = C.CUIIO_VERS
+                );
