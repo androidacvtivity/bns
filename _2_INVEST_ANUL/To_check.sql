7e319f55f310ff0fdd18597b735e8ff7a2a9608c
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-07-16 15:47:55 +0300

    update

---
+++ _2_INVEST_ANUL/To_check.sql
@@ -0,0 +1,20 @@
+﻿           SELECT 
+            D.CUIIO,
+            D.RIND,
+            SUM(D.COL1) AS COL1, 
+            SUM(D.COL2) AS COL2
+            
+                FROM    CIS.VW_DATA_ALL D
+                
+                WHERE 
+                
+                D.capitol = 31 AND D.capitol_vers=1040
+                AND D.PERIOADA IN (1044,1045,1046,1047)
+                AND D.RIND IN ('100')
+                
+                GROUP BY 
+                D.CUIIO,
+                D.RIND 
+                
+           HAVING       
+                SUM(NVAL(D.COL1) + NVAL(D.COL2)) > 0
