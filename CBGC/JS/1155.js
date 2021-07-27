7df53467ee1b5339ee13844fb9ec8c8c36cd3596
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-07-27 10:50:22 +0300

    update

---
+++ CBGC/JS/1155.js
@@ -0,0 +1,310 @@
+var from = "";
+var msgErr = "Nu ati introdus o valoare corecta in coloana.";
+
+$(document).ready(function () {
+    form = $("#formDenShort").val();
+    fCBGC_Cap9_2();
+});
+$(function () {
+    $("input:not([type='button']):not([disabled])").on("change", function (e) {
+        fCBGC_Cap9_2();
+    });
+});
+
+function fCBGC_Cap9_2() { 
+
+
+    var R1_C1 = $("#68_1155_63264_1_1");
+    var R1_C2 = $("#68_1155_63264_1_2");
+    var R2_C1 = $("#68_1155_63265_2_1");
+    var R2_C2 = $("#68_1155_63265_2_2");
+    var R3_C1 = $("#68_1155_63266_3_1");
+    var R3_C2 = $("#68_1155_63266_3_2");
+    var R4_C1 = $("#68_1155_63268_4_1");
+    var R4_C2 = $("#68_1155_63268_4_2");
+    var R5_C1 = $("#68_1155_63269_5_1");
+    var R5_C2 = $("#68_1155_63269_5_2");
+    var R6_C1 = $("#68_1155_63270_6_1");
+    var R6_C2 = $("#68_1155_63270_6_2"); 
+    var R7_C1 = $("#68_1155_63271_7_1");
+    var R7_C2 = $("#68_1155_63271_7_2"); 
+    var R8_C1 = $("#68_1155_63272_8_1");
+    var R8_C2 = $("#68_1155_63272_8_2"); 
+    var R9_C1 = $("#68_1155_63274_9_1");
+    var R9_C2 = $("#68_1155_63274_9_2"); 
+    var R10_C1 = $("#68_1155_63275_10_1");
+    var R10_C2 = $("#68_1155_63275_10_2"); 
+    var R11_C1 = $("#68_1155_63277_11_1");
+    var R11_C2 = $("#68_1155_63277_11_2"); 
+    var R12_C1 = $("#68_1155_63279_12_1");
+    var R12_C2 = $("#68_1155_63279_12_2"); 
+    var R13_C1 = $("#68_1155_63280_13_1");
+    var R13_C2 = $("#68_1155_63280_13_2"); 
+    var R14_C1 = $("#68_1155_63281_14_1");
+    var R14_C2 = $("#68_1155_63281_14_2");
+    var R15_C1 = $("#68_1155_63282_15_1");
+    var R15_C2 = $("#68_1155_63282_15_2");
+    var R16_C1 = $("#68_1155_63283_16_1");
+    var R16_C2 = $("#68_1155_63283_16_2");
+    var R17_C1 = $("#68_1155_63285_17_1");
+    var R17_C2 = $("#68_1155_63285_17_2");
+    var R18_C1 = $("#68_1155_63286_18_1");
+    var R18_C2 = $("#68_1155_63286_18_2");
+    var R19_C1 = $("#68_1155_63287_19_1");
+    var R19_C2 = $("#68_1155_63287_19_2");
+    
+    
+    
+    
+     if(R1_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R1_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R1_C2.prop("readonly", true));
+}  
+   else{
+ eval(R1_C2.prop("readonly", false));
+}
+  
+    if(R2_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R2_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R2_C2.prop("readonly", true));
+}  
+   else{
+ eval(R2_C2.prop("readonly", false));
+}
+
+
+
+if(R3_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R3_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R3_C2.prop("readonly", true));
+}  
+   else{
+ eval(R3_C2.prop("readonly", false));
+}
+
+
+if(R4_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R4_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R4_C2.prop("readonly", true));
+}  
+   else{
+ eval(R4_C2.prop("readonly", false));
+}
+
+
+if(R5_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R5_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R5_C2.prop("readonly", true));
+}  
+   else{
+ eval(R5_C2.prop("readonly", false));
+}
+
+
+if(R6_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R6_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R6_C2.prop("readonly", true));
+}  
+   else{
+ eval(R6_C2.prop("readonly", false));
+}
+
+
+if(R7_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R7_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R7_C2.prop("readonly", true));
+}  
+   else{
+ eval(R7_C2.prop("readonly", false));
+}
+
+
+
+
+if(R8_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R8_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R8_C2.prop("readonly", true));
+}  
+   else{
+ eval(R8_C2.prop("readonly", false));
+}
+
+
+
+if(R9_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R9_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R9_C2.prop("readonly", true));
+}  
+   else{
+ eval(R9_C2.prop("readonly", false));
+}
+
+
+
+if(R10_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R10_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R10_C2.prop("readonly", true));
+}  
+   else{
+ eval(R10_C2.prop("readonly", false));
+}
+
+
+
+if(R11_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R11_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R11_C2.prop("readonly", true));
+}  
+   else{
+ eval(R11_C2.prop("readonly", false));
+}
+
+
+
+if(R12_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R12_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R12_C2.prop("readonly", true));
+}  
+   else{
+ eval(R12_C2.prop("readonly", false));
+}
+
+
+
+
+if(R13_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R13_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R13_C2.prop("readonly", true));
+}  
+   else{
+ eval(R13_C2.prop("readonly", false));
+}
+
+
+
+if(R14_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R14_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R14_C2.prop("readonly", true));
+}  
+   else{
+ eval(R14_C2.prop("readonly", false));
+}
+
+
+if(R15_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R15_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R15_C2.prop("readonly", true));
+}  
+   else{
+ eval(R15_C2.prop("readonly", false));
+}
+
+
+if(R16_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R16_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R16_C2.prop("readonly", true));
+}  
+   else{
+ eval(R16_C2.prop("readonly", false));
+}
+
+
+if(R17_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R17_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R17_C2.prop("readonly", true));
+}  
+   else{
+ eval(R17_C2.prop("readonly", false));
+}
+
+
+
+if(R18_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R18_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R18_C2.prop("readonly", true));
+}  
+   else{
+ eval(R18_C2.prop("readonly", false));
+}
+
+
+
+
+
+if(R19_C1.val() == "2"){
+//----------clear input --------------------------
+
+eval(R19_C2.val(""));
+ //----------readOnly input -----------------------
+eval(R19_C2.prop("readonly", true));
+}  
+   else{
+ eval(R19_C2.prop("readonly", false));
+}
+
+    
+    
+ 
+
+
+
+  
+
+}
+
+
+
