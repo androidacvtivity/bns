0440cafceae5c3a483b04d56ac30ceab1799b47e
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-08-18 07:59:05 +0300

    update

--- _2_CMPA/NaN.sql
+++ _2_CMPA/NaN.sql
@@ -2,6 +2,7 @@
         D.UNIT_CODE,
         D.CAPITOL,
         D.RIND,
+        MR.ID_MD,
         D.COL1,
     D.COL2,
     D.COL3,
@@ -25,14 +26,17 @@
                
 FROM
   VW_DATA_ALL_GC  D
+            INNER JOIN MD_RIND MR ON MR.ID_MD = D.ID_MD 
 WHERE
   (D.PERIOADA IN (:pPERIOADA))  AND   
   (D.FORM =:pFORM) AND
   (D.FORM_VERS =:pFORM_VERS) AND 
   (:pID_MDTABLE =:pID_MDTABLE) AND 
-  (D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%') AND
-  D.FORM IN (58)  AND
-  D.CAPITOL IN (416)
+  (D.CUATM_FULL LIKE '%'||:pCOD_CUATM||';%') 
+  
+  AND
+  D.FORM IN (62)  
+ -- D.CAPITOL IN (416)
   
   --AND D.UNIT_CODE =  168386 
 
@@ -42,6 +46,7 @@ WHERE
  D.UNIT_CODE,
         D.CAPITOL,
         D.RIND,
+         MR.ID_MD,
         D.COL1,
     D.COL2,
     D.COL3,
