73f299d6a48c12dabfd12cf1c780c01b0160a7ae
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-11-11 13:42:59 +0200

    INSERT INTO CIS2.MD_TABLES
    INSERT INTO CIS2.MD_TABLES

---
+++ _1_EDU/1-edu 2021_ST-20-0011-Mod-0001/table_out/MD_TABLE_40_2010.sql
@@ -0,0 +1,76 @@
+﻿--INSERT INTO CIS2.MD_TABLES
+--(
+--  ID_MDTABLE,  
+--  TABELE,      
+--  TABELE_VERS, 
+--  FORM,        
+--  FORM_VERS,   
+--  DENUMIRE,
+--  DEN_SHORT,
+--  NIVELRAIONAL,
+--  ORDINE,
+--  DATA_REG,
+--  STATUT,
+--  HTML_HEADER,
+--  SQL_TEXT,
+--  SQL_TEXT1,
+--  LANDSCAPE,
+--  COL_NUM,
+--  FOOTNOTE,
+--  UM,
+--  CREATING    
+--        
+--)
+
+SELECT 
+  ID_MDTABLE,  
+  TABELE,      
+  2010 TABELE_VERS, 
+  FORM,        
+  FORM_VERS,   
+  DENUMIRE,
+  DEN_SHORT,
+  NIVELRAIONAL,
+  ORDINE,
+  DATA_REG,
+  STATUT,
+  HTML_HEADER,
+  SQL_TEXT,
+  SQL_TEXT1,
+  LANDSCAPE,
+  COL_NUM,
+  FOOTNOTE,
+  UM,
+  CREATING    
+        
+        
+                FROM CIS2.MD_TABLES
+                
+                WHERE 
+                
+                FORM   = 40
+                
+                
+                AND ID_MDTABLE IN 
+
+  (SELECT D.ID_MDTABLE
+    FROM CIS2.MD_TABLES D
+         INNER JOIN (  SELECT D.TABELE, MAX (D.TABELE_VERS) TABELE_VERS
+                         FROM CIS2.MD_TABLES D
+                        WHERE D.FORM = 40
+                     GROUP BY D.TABELE) DD
+             ON (D.TABELE = dd.TABELE AND D.TABELE_VERS = dd.TABELE_VERS)
+   WHERE D.FORM = 40
+GROUP BY D.ID_MDTABLE,
+         D.TABELE,
+         D.TABELE_VERS,
+         d.ORDINE
+)
+
+
+AND TABELE_VERS <> 2010
+
+ORDER BY 
+ORDINE
+                
+                
