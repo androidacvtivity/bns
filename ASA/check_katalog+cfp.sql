022027e44d69927e39f1673b70c26526d8cf9086
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-11-16 09:13:43 +0200

    Tab.1  (*)   ASA
    Tab.1  (*)  ASA

---
+++ ASA/check_katalog+cfp.sql
@@ -0,0 +1,16 @@
+    SELECT 
+    DISTINCT CFP 
+        FROM USER_BANCU.VW_KATALOG_ASA_2009
+        
+        WHERE 
+        1=1
+       -- CFP IN ('00','10','11','14','16')
+        
+--        CFP IN (
+--          SELECT 
+--        
+--        DISTINCT GRUPA  
+--        
+--
+--       FROM  CIS2.VW_CL_CFP
+--        )
