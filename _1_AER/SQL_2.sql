0ebfc162a92d1f98ac51eff8ea371be0596e51e9
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-12-01 11:33:19 +0200

    SQL 1 AER
    SQL 1 AER

---
+++ _1_AER/SQL_2.sql
@@ -0,0 +1,64 @@
+
+
+SELECT 
+     RIND, 
+     SUM(COL6) AS COL6
+        
+                FROM CIS2.VW_DATA_ALL
+                
+                  WHERE 
+                
+                FORM = 38
+                
+                AND PERIOADA = 2008
+                
+             AND    CAPITOL = 389 AND capitol_vers = 2000
+AND RIND IN ('0001') 
+
+AND COL6 IS NOT NULL
+
+AND CUIIO IN (
+
+SELECT DISTINCT FC.CUIIO
+FROM 
+
+         (   
+         
+         SELECT FC.CUIIO,
+                   FC.CUIIO_VERS,
+                   FC.FORM,
+                   FC.FORM_VERS,
+                   FC.STATUT
+              FROM CIS2.FORM_CUIIO  FC
+                   INNER JOIN (  SELECT CUIIO, MAX (CUIIO_VERS) CUIIO_VERS
+                                   FROM CIS2.FORM_CUIIO
+                                  WHERE FORM IN (38) AND CUIIO_VERS <= 2009
+                               GROUP BY CUIIO) BB
+                       ON (    BB.CUIIO = FC.CUIIO
+                           AND BB.CUIIO_VERS = FC.CUIIO_VERS)
+             WHERE FC.FORM IN (38) AND FC.STATUT <> '3'
+             
+             
+          
+             )    FC  LEFT JOIN (
+             
+                 SELECT 
+    DISTINCT CUIIO
+        
+                FROM CIS2.VW_DATA_ALL
+                
+                WHERE 
+                
+                FORM = 38
+                
+                AND PERIOADA = 2009
+             
+             ) R ON FC.CUIIO = R.CUIIO
+             
+             WHERE 
+             R.CUIIO IS NULL
+)
+
+
+GROUP BY 
+RIND
