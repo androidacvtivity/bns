94294d8445dbbeb61ad601ad1d75cd6b0cd03655
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-12-01 19:42:39 +0200

    Tab.7.A.4

---
+++ _1_EDU/1-edu 2021_ST-20-0011-Mod-0001/table_out/CIS2.VW_CLS_CLASS_ITEM.sql
@@ -0,0 +1,7 @@
+﻿SELECT *
+                
+                FROM CIS2.MD_TABLES
+                
+                        WHERE 
+                        
+                        SQL_TEXT LIKE '%VW_CLS_CLASS_ITEM%'
