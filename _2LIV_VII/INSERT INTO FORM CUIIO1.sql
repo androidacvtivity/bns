450e8bd129de373834f7d56e447b70d3851a585c
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-12-08 11:54:53 +0200

    CAT 2-Liv-Vii (ian-decembrie) 2021 #478

---
+++ _2LIV_VII/INSERT INTO FORM CUIIO1.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO CIS2.FORM_CUIIO (
+
+            CUIIO,
+            CUIIO_VERS,
+            FORM,
+            FORM_VERS,
+            STATUT
+            
+
+
+)
+
+
+
+           SELECT 
+            CUIIO,
+            CUIIO_VERS,
+            37 FORM,
+            1800 FORM_VERS,
+            '1' STATUT
+            
+                FROM CIS2.RENIM
+                
+                WHERE 
+                
+               
+                
+                CUIIO IN (
+37465937,
+38987084,
+40113702,
+40227340,
+40350951,
+40419660,
+40430331,
+40582029,
+40600224,
+40749972,
+40902379,
+40967522,
+40976805,
+41190751,
+41342089,
+41407191,
+41478329,
+4129625492
+
+
+)
+
+AND CUIIO_VERS IN (1823)
