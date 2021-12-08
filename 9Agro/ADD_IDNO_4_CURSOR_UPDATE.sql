d89de01e2944077e635ddfb16300784514357b48
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-12-08 09:32:09 +0200

    Catalogul 9-AGR cu coduri IDNO #474
    Catalogul 9-AGR cu coduri IDNO #474

---
+++ 9Agro/ADD_IDNO_4_CURSOR_UPDATE.sql
@@ -0,0 +1,37 @@
+﻿DECLARE -- ====================================================================
+
+  CURSOR C IS
+        
+              SELECT 
+            L.CUIIO,
+            L.CUIIO_VERS,
+            L.IDNO NEW_IDNO,
+            R.IDNO
+               FROM  USER_BANCU.KAT_9_AGRO L
+                        LEFT JOIN CIS2.RENIM R ON R.CUIIO = L.CUIIO  
+                        AND R.CUIIO_VERS = L.CUIIO_VERS 
+                        
+                        
+                        WHERE 
+                        1=1   
+                           
+                        AND R.IDNO IS NULL
+                        ORDER BY
+                        R.IDNO
+          
+          
+  ;
+
+BEGIN -- ======================================================================
+  FOR CR IN C
+  LOOP
+    UPDATE RENIM SET 
+      IDNO = CR.NEW_IDNO
+    WHERE 
+      CUIIO = CR.CUIIO AND
+      CUIIO_VERS = CR.CUIIO_VERS 
+      
+      
+    ;
+  END LOOP;
+END; -- =======================================================================
