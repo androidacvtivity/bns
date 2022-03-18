9833484a514afd3dc09358e6e54f40fd0fe9b40a
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-03-18 15:10:12 +0200

    Nr.2 "Salubrizarea localității" #865
    Nr.2 "Salubrizarea localității" #865

---
+++ _1_CHELTUIELI/add_units.sql
@@ -0,0 +1,14 @@
+    SELECT *
+    
+        FROM USER_BANCU.VW_MAX_RENIM_CIS2
+
+        
+        WHERE 
+         1=1 
+         
+         AND CUIIO IN (
+41466584
+
+         )
+         
+         
