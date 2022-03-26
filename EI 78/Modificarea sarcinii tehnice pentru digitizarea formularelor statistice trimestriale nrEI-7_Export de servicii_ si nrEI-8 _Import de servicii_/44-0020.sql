309c91ca63b829c54e0c99070fe2c34acb245777
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-03-26 17:46:05 +0200

    daily updating

---
+++ EI 78/Modificarea sarcinii tehnice pentru digitizarea formularelor statistice trimestriale nrEI-7_Export de servicii_ si nrEI-8 _Import de servicii_/44-0020.sql
@@ -0,0 +1,70 @@
+SELECT            
+          'Rind '||D.RIND||' Cod AEE ['|| MAX(CASE WHEN 1=1 THEN  
+          
+          (CASE WHEN D.COL31 IS NULL THEN 'Nu este completat' ELSE D.COL31 END)  
+          
+          
+          ELSE NULL  END )  ||'] <> [' ||  
+            MAX(CASE WHEN 1=1 THEN  
+            
+            (CASE 
+            
+            WHEN TO_CHAR(D.COL3) IS NULL   THEN 'Nu este completat' ELSE TO_CHAR(D.COL3) END 
+            
+            )  
+            
+            
+            ELSE TO_CHAR(NULL)  END)  || '] Cod ?SPM' 
+
+   
+ AS REZULTAT
+
+FROM
+  CIS2.VW_DATA_ALL D  
+ 
+
+WHERE
+  (D.PERIOADA=:PERIOADA          ) AND
+  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               OR :FORM = -1) AND
+  (D.FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (44)  AND
+  D.CAPITOL IN (405) 
+  
+  
+ 
+
+GROUP BY 
+    D.RIND,
+    D.COL31,
+    D.COL3
+    
+    
+    HAVING 
+    
+    ( 
+      MAX(CASE WHEN 1=1 THEN  D.COL31 ELSE NULL  END)  IS NOT NULL   
+      
+       AND 
+      
+      SUM(CASE WHEN 1=1  THEN  CIS2.NVAL(D.COL3) ELSE 0  END) = 0 
+      
+      )
+      
+
+
+OR 
+
+    ( 
+      MAX(CASE WHEN 1=1 THEN  D.COL31 ELSE NULL  END)  IS  NULL   
+      
+       AND 
+      
+      SUM(CASE WHEN 1=1  THEN  CIS2.NVAL(D.COL3) ELSE 0  END) > 0 
+      
+      )
