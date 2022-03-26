10ca050754b372fda57ed9a6047321992105c9b5
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-03-26 18:58:12 +0200

    44-0031

---
+++ EI 78/Modificarea sarcinii tehnice pentru digitizarea formularelor statistice trimestriale nrEI-7_Export de servicii_ si nrEI-8 _Import de servicii_/44-0031.sql
@@ -0,0 +1,129 @@
+﻿SELECT            
+         'Conditia nu este satisfacuta'
+
+ AS REZULTAT
+
+FROM
+  CIS2.VW_DATA_ALL D  
+ 
+ 
+WHERE
+  (D.PERIOADA=:PERIOADA          ) AND
+  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               OR :FORM = -1) AND
+  (D.FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
+  (:CAPITOL=:CAPITOL            OR :CAPITOL<>:CAPITOL) AND
+  (:CAPITOL_VERS=:CAPITOL_VERS  OR :CAPITOL_VERS <> :CAPITOL_VERS) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (44)  AND
+  D.CAPITOL IN (405,407 )
+  
+  GROUP BY 
+    D.CUIIO
+
+    
+    HAVING 
+    
+    (
+    (
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.10') THEN  1 ELSE 0  END) )) >  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.10') THEN  1 ELSE 0  END)   )   =  0
+     
+     )
+    
+
+OR 
+
+(
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.10') THEN  1 ELSE 0  END) ))  =  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.10') THEN  1 ELSE 0  END)   )   >  0
+     
+     )
+    )
+-----------------------------------------------------------    
+OR 
+(
+    (
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.11') THEN  1 ELSE 0  END) )) >  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.11') THEN  1 ELSE 0  END)   )   =  0
+     
+     )
+    
+
+OR 
+
+(
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.11') THEN  1 ELSE 0  END) ))  =  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.11') THEN  1 ELSE 0  END)   )   >  0
+     
+     )
+    )
+    ---------------------------------------------------------
+    
+    OR 
+(
+    (
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.12') THEN  1 ELSE 0  END) )) >  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.12') THEN  1 ELSE 0  END)   )   =  0
+     
+     )
+    
+
+OR 
+
+(
+    
+    (MAX(CASE WHEN D.CAPITOL IN (405) AND D.COL31 IN  ('49.39.12') THEN  1 ELSE 0  END) ))  =  0
+       
+
+    
+    AND 
+     
+    (
+     
+     (SUM(CASE WHEN D.CAPITOL IN (407) AND D.COL31 IN  ('49.39.12') THEN  1 ELSE 0  END)   )   >  0
+     
+     )
+    )
