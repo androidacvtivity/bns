0995e74a0b5df19b1ac441df581e651c1dfbef19
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-03-27 18:27:26 +0300

    44-0019

---
+++ EI 78/Modificarea sarcinii tehnice pentru digitizarea formularelor statistice trimestriale nrEI-7_Export de servicii_ si nrEI-8 _Import de servicii_/CL_TARI_RIND_.sql
@@ -0,0 +1,30 @@
+insert into CIS2.MD_RIND_OUT(
+ID_MDTABLE,
+RINDOUT_VERS,
+RINDOUT,
+DENUMIRE, 
+ORDINE,
+DATA_REG, 
+RIND  
+
+)
+
+
+SELECT 
+  12100 ID_MDTABLE,
+  1052 RINDOUT_VERS,
+  ISO1 RINDOUT,
+  DENUMIRE DENUMIRE, 
+  ORDINE,
+  SYSDATE DATA_REG, 
+  CODUL RIND   
+    
+
+
+        
+            FROM USER_BANCU.CL_TARI_MODIF
+            
+            
+            
+            
+            
