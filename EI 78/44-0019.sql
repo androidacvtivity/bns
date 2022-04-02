54f48f7a9e5b30795dfb6adf36ed53af53c0bd7a
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-04-02 09:52:27 +0300

    44-0032

---
+++ EI 78/44-0019.sql
@@ -0,0 +1,49 @@
+SELECT 
+
+'Rind. '||L.RIND||' - nu exista asa cod in clasificatorul tarilor = ['||L.COL33||']'
+ AS REZULTAT
+
+
+FROM
+(
+SELECT   
+D.RIND,
+D.COL33
+   FROM
+  CIS2.VW_DATA_ALL_TEMP D  
+ 
+
+WHERE
+  (D.PERIOADA=:PERIOADA          ) AND
+  (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
+  (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+  (D.FORM = :FORM               OR :FORM = -1) AND
+  (D.FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
+  (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+  (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+  (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+  
+  D.FORM IN (44)  AND
+  D.RIND NOT IN ('1') AND 
+  D.CAPITOL IN (405) 
+  
+GROUP BY 
+  D.RIND, 
+  D.COL33
+
+) L LEFT JOIN (
+
+  SELECT D.RIND CODUL
+FROM CIS2.MD_RIND_OUT D
+
+WHERE
+ D.ID_MDTABLE = 12100
+ 
+ ORDER BY
+ D.ORDINE
+) R ON L.COL33 = R.CODUL
+ 
+
+WHERE 
+
+R.CODUL IS NULL
