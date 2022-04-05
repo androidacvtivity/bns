b08a64ff41c53d2b06d18d96018ba5e350eede66
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-04-05 11:19:37 +0300

    info CRSmC SQL

--- EI 78/44-0001.sql
+++ EI 78/44-0001.sql
@@ -6,7 +6,7 @@ FROM
     SELECT
       D.CUIIO
     FROM
-      CIS2.VW_DATA_ALL_TEMP D
+      CIS2.VW_DATA_ALL D
     WHERE
       (D.PERIOADA=:PERIOADA          ) AND
       (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
@@ -18,6 +18,24 @@ FROM
       :FORM = :FORM AND
        D.FORM IN (44)  AND
       D.CAPITOL IN(1,14)
+      
+     UNION 
+     SELECT
+      D.CUIIO
+    FROM
+      CIS2.VW_DATA_ALL D
+    WHERE
+      (D.PERIOADA=:PERIOADA          ) AND
+      (D.CUIIO=:CUIIO               OR :CUIIO = -1) AND
+      (D.CUIIO_VERS=:CUIIO_VERS     OR :CUIIO_VERS = -1) AND
+      (D.FORM_VERS=:FORM_VERS       OR :FORM_VERS = -1) AND
+      (D.CAPITOL_VERS=:CAPITOL_VERS OR :CAPITOL_VERS = -1) AND
+      (D.ID_MD=:ID_MD               OR :ID_MD = -1) AND
+      (D.CAPITOL=:CAPITOL           OR :CAPITOL = -1) AND
+      :FORM = :FORM AND
+       D.FORM IN (44)  AND
+      D.CAPITOL IN(405,407)
+      
   ) D ON(1=1)
 WHERE
   D.CUIIO IS NULL
