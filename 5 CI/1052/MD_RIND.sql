5be470559154fa7c6960e21043c2c46b805ba726
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-04-09 13:12:00 +0300

    Update MD_RIND.sql

--- 5 CI/1052/MD_RIND.sql
+++ 5 CI/1052/MD_RIND.sql
@@ -1,31 +1,9 @@
---UPDATE  MD_RIND
---
---SET DENUMIRE =  REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(DENUMIRE,'a','a'),'s','s'),'t','t'),'î','i'),'â','a'),'?','s') 
 
-SELECT 
-        RIND,
-        DENUMIRE,
-        REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(DENUMIRE,'a','a'),'s','s'),'t','t'),'î','i'),'â','a'),'?','s') 
-        AS DENUMIRE
 
-        
-        FROM MD_RIND
-        
-        WHERE 
-        
-        FORM = 1
-        
-        
-        AND 
-        
-        (-
-        DENUMIRE LIKE '%a%'
-        or 
-        DENUMIRE LIKE '%s%'
-        ) 
-        
---        
---        
---        
---        ORDER BY 
---        ID_MD
+SELECT *
+
+ FROM CIS.MD_RIND
+ 
+ WHERE 
+ FORM = 1
+AND CAPITOL_VERS = 1052 
