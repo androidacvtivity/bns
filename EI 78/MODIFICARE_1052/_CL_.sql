3b3089ec436d8f3cb34547a68a62200b28e6d1ec
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2022-04-27 13:10:13 +0300

    Daily update

---
+++ EI 78/MODIFICARE_1052/_CL_.sql
@@ -0,0 +1,9 @@
+    SELECT D.*
+        
+            FROM CIS2.MD_TABLES D
+            
+            WHERE 
+            
+            D.SQL_TEXT LIKE '%CLS_CLASS_ITEM%'
+            
+            
